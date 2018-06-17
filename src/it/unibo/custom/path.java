f26447c1a234a1cd0c181f31e5da37535fa986bc
Robotex <robotex@altervista.org>
2018-06-17 03:54:57 +0200

    Aggiunto quarto parametro REGISTER a moveRover, risolto NULL exception
    NULL exception si genera perché i comandi mandati venivano registrati
    nuovamente e questo invalidava l'iteratore

--- src/it/unibo/custom/path.java
+++ src/it/unibo/custom/path.java
@@ -12,49 +12,68 @@ import alice.tuprolog.Term;
 import cli.System.TimeSpan;
 
 import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class path {
 	
-	static Stack<Action> stack = new Stack<Action>();
+	static Deque<Action> stack = new ArrayDeque<Action>();
 	
-	public static class Action{
+	public static class Action {
 		public String move;
 		public String time;
 		private long timeStamp;
 		private long actualDuration;
-		public Action(String move, String time){
+		
+		public Action(Action other) {
+			this.move = other.move;
+			this.time = other.time;
+			this.timeStamp = other.timeStamp;
+			this.actualDuration = other.actualDuration;
+		}
+		
+		public Action(String move, String time) {
 			this.move = move;
 			this.time = time;
 			this.timeStamp = System.currentTimeMillis();			
 		}
 		
-		public Action(String move, String time, String timeout){
+		public Action(String move, String time, String timeout) {
 			this.move = move;
 			this.time = time;
 			this.timeStamp = System.currentTimeMillis();
-			if(time == "0" || time.isEmpty())
+			if (time.isEmpty() || time == "0")
 				this.actualDuration = Long.parseLong(timeout);			
 			
 		}
 		public String toString(){
-			return move + "("+time+")";
+			return getCmd();
 		}
 		public Action reverse() {
-			Action reversed = this;
-			if (this.move == "left")
+			Action reversed = new Action(this);
+			if (reversed.move.equals("left"))
 				reversed.move = "right";
-			else if (this.move == "right")
+			else if (reversed.move.equals("right"))
 				reversed.move = "left";
 			return reversed;
 		}
 		
 		public String formatAsMessage(Prolog prologEngine){
 			//return QActorUtils.unifyMsgContent(prologEngine, this.move +"(TIME)", this.move + "(" + this.time + ")", null ).toString();	
-			String parg="cmd(move(\"" + this.move + "\",20," + this.actualDuration + "))";
+			String parg=getCmd();
 			//parg = updateVars(Term.createTerm("usercmd(executeInput(X))"),  Term.createTerm("usercmd(executeInput(move(X,Y,Z)))"), 
   			//		Term.createTerm(currentEvent.getMsg()), parg);
+			return parg;
+		}
+		
+		public String getCmd() {
+			String parg;
+			if (this.actualDuration > 0)
+				parg="cmd(move(\"" + this.move + "\",20," + this.actualDuration + "))";
+			else
+				parg="cmd(move(\"" + this.move + "\",20," + this.time + "))";
 			return parg;
 		}
 	}
@@ -67,37 +86,41 @@ public class path {
 			Action lastAction = stack.peek();
 			lastAction.actualDuration = System.currentTimeMillis() - lastAction.timeStamp; 
 		}
+		
+		myself.println("[console] register: " + action.getCmd() + " reversed: " + action.reverse().getCmd());
 
-		stack.add(action);		
+		stack.push(action);		
 	}
 	public static void register(QActor myself, String move, String time, String timeout) {
 		Action action = new Action(move, time, timeout);		
 		if(action.move == "stop" && stack.size() > 0){
-			Action lastAction = stack.get(stack.size() - 1);
+			Action lastAction = stack.peek();
 			lastAction.actualDuration = System.currentTimeMillis() - lastAction.timeStamp; 
 		}
-		stack.add(action);
+		stack.push(action);
 	}
 	
-	public static void startReverse(QActor myself) throws Exception{
-		System.out.println("starting reverse" );
-		Iterator<Action> iterator = stack.iterator();
-		Runnable reverse = () -> { 
+	public static void startReverse(QActor myself){
+
 			try {	
-				myself.sendMsg("moveRover", "rover", QActorContext.dispatch, "cmd(move(\"right\",20,750))");
-				myself.sendMsg("moveRover", "rover", QActorContext.dispatch, "cmd(move(\"right\",20,750))");
+				myself.println("[console] startReverse rotating rover");
+				
+				myself.sendMsg("moveRover", "rover", QActorContext.dispatch, "cmd(move(right,20,750))");
 				Thread.sleep(1500);
-			} catch (Exception e) {	}
+				myself.sendMsg("moveRover", "rover", QActorContext.dispatch, "cmd(move(right,20,750))");
+				Thread.sleep(1500);
+				
+				myself.println("[console] startReverse executing path");
 			
-			while(iterator.hasNext()){
-				Action curraction = iterator.next().reverse();
-				System.out.println("sending:" + curraction.toString());
-				try {					
-					myself.sendMsg("moveRover", "rover", QActorContext.dispatch, curraction.formatAsMessage(myself.getPrologEngine()));
-					Thread.sleep(curraction.actualDuration);
-				} catch (Exception e) {	}
-			}	
-		};
-		reverse.run();		
+				for (Action curraction : stack) {
+					Action reversedaction = curraction.reverse();
+
+						myself.sendMsg("moveRover", "rover", QActorContext.dispatch, reversedaction.formatAsMessage(myself.getPrologEngine()));
+						Thread.sleep(reversedaction.actualDuration);
+				}		
+			
+			} catch (Exception e) {	
+				myself.println("[console] startReverse Exception! " + e.getLocalizedMessage());
+			}
 	}
 }
